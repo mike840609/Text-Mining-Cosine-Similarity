1b24804fe113077d65429415b67858336397fd25
DESKTOP-BNH19L5\ianchen <mike840609@gmail.com>
2017-12-04 00:37:08 +0800

    done

--- Model/TrainingClassHolder.py
+++ Model/TrainingClassHolder.py
@@ -12,7 +12,7 @@ class TrainingClassHolder:
     #  store traing document 
     # key = class number , value = doc id list
     classDicID = {}
-
+    
     # key = class number
     classDocFrequency = {}
 
@@ -22,7 +22,17 @@ class TrainingClassHolder:
     # generate form for each term 
     classTermScore = {}
 
-    featureTerm= {}
+    featureTerm_tumple = {}
+    featureTerm_list = []
+
+    #  naive bayes text classification 
+    all_featureTerm_frequency_in_class = {}
+
+    class_term_frequenct = {}
+    class_probability_score = {}
+    
+    testing_record = {}
+
 
     def __init__(self,path):
 
@@ -30,7 +40,6 @@ class TrainingClassHolder:
             items = line.split()
             self.classDicID[items[0]] = items[1:]
             # print(self.classDic[items[0]])
-
 
     # select feature term 
     def selectFeature(self,docList_obj):
@@ -96,18 +105,81 @@ class TrainingClassHolder:
                 score = -2 * math.log(((pt)**n11 * (1-pt)**n10 * (pt)**n01 * (1-pt)**n00) / ((p1**n11) * (1-p1)**n10 * ( p2**n01) *((1-p2)**n00)))
                 self.classTermScore[key][classId]["score"] = score
         
-        # term select 
+        # term select =========================================================================================================== 
         for key in self.trainingDocFrequency:
-            self.featureTerm[key] = 0
+            self.featureTerm_tumple[key] = int(0)
             for classId in self.classDicID:
-                self.featureTerm[key] += self.classTermScore[key][classId]["score"]                                 
+                self.featureTerm_tumple[key] += self.classTermScore[key][classId]["score"]                                 
         
-        self.featureTerm = sorted(self.featureTerm.items(), key= lambda x : x[1] , reverse=True)
-        print( self.featureTerm)
+        self.featureTerm_tumple = sorted(self.featureTerm_tumple.items(), key= lambda x : x[1] , reverse=True)
+
+        for i in range(500):
+            self.featureTerm_list.append( self.featureTerm_tumple[i][0])
+        
+
+        # training ===================================================================================================
+        
+        for classId in self.classDicID:
+            self.class_term_frequenct[classId] = dict()
+            
+            for docId in self.classDicID[classId]:
+
+                doc_vocabulary = [doc for doc in docList_obj if doc.id == docId]
+                
+                for term in doc_vocabulary[0].obj.results : 
+                    if term in self.featureTerm_list:
+                        if self.class_term_frequenct[classId].get(term , 0 ) == 0:
+                            self.class_term_frequenct[classId][term] = 1
+                        else:   
+                            self.class_term_frequenct[classId][term] += 1
+        
+        for classId in self.classDicID:
+            self.all_featureTerm_frequency_in_class = sum(self.class_term_frequenct[classId].values())
+            # print (self.all_featureTerm_frequency_in_class)  
+
+        # calculate probability 
+        for term in self.featureTerm_list:
+            self.class_probability_score[term] = dict()
+
+            for classId  in self.classDicID:
+                
+                probability = math.log(((1 + self.class_term_frequenct[classId].get(term , 0 ))/(500 + self.all_featureTerm_frequency_in_class)))
+                
+                self.class_probability_score[term][classId] =  probability 
+
+        # print (json.dumps(self.class_probability_score))
+
+        # testing ===================================================================================================
+        training_set = []
+        for classId in self.classDicID:
+            for docId in self.classDicID[classId]:
+                training_set.append(docId)
         
 
 
-        # print (json.dumps(self.classTermScore, indent=2))
+        for doc in  docList_obj :
+            if doc.id not in training_set:
+
+                self.testing_record[doc.id] = dict()
+
+                word_counter = dict(Counter(doc.obj.results))
+
+                for classId in self.classDicID:
+
+                    self.testing_record[doc.id][classId] =  0 
+                    class_score = 0 
+
+                    for term in self.featureTerm_list:
+                        class_score += self.class_probability_score[term][classId] * word_counter.get(term , 0 )
+                        self.testing_record[doc.id][classId] = class_score    
+
+
+
+                    
+                
+
+
+        print (json.dumps(self.testing_record, indent=2))
         # print (self.classTermScore)
 
 
